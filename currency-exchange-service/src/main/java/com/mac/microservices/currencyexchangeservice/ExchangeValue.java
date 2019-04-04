99401d9ebf499dd92a46bb145873f13926b80756
Jenkins <jaya.maduka@gmail.com>
2019-04-05 01:19:00 +0800

    Configure JPA and Initialized Data

--- currency-exchange-service/src/main/java/com/mac/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/mac/microservices/currencyexchangeservice/ExchangeValue.java
@@ -5,13 +5,22 @@ package com.mac.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author Administrator
  *
  */
+@Entity
 public class ExchangeValue {
+	@Id
 	private Long id;
+	
+	@Column(name = "currency_from")
 	private String from;
+	@Column(name = "currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private int port;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple,port) values (1001, 'USD', 'LKR', 165,0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple,port) values (1002, 'EUR', 'LKR', 180,0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple,port) values (1003, 'AUD', 'LKR', 140,0);
