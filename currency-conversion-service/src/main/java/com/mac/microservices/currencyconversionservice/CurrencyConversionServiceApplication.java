edecee304c314033035137e3e69a066105b50762
Jenkins <jaya.maduka@gmail.com>
2019-04-09 23:55:15 +0800

    Connecting Currency Conversion Microservice to Eureka Changed the boot parent version due to following issue Note : SpringBoot 2.2 will be supported by the upcoming Spring Cloud Hoxton Release Train. For now, with Greenwich, please use Boot 2.1. spring-cloud/spring-cloud-netflix#3410

--- currency-conversion-service/src/main/java/com/mac/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/mac/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.mac.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.mac.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
